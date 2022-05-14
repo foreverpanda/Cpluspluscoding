8ddd59b396f7ad45588c3a3f39956322fd35db9b
foreverpanda <85871209+foreverpanda@users.noreply.github.com>
2022-05-14 21:07:00 +0530

    Create code10.cpp

---
+++ Day1/pattern/code10.cpp
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,temp;
+    
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j =1;
+        while(j<=i)
+        {
+            cout<<" * ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+    /*
+Enter number of rows : 4
+ * 
+ *  * 
+ *  *  * 
+ *  *  *  * 
+
+*/
+
+}
