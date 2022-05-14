2d93dba6c972999f3d1826bbc579badff1a63ac8
foreverpanda <85871209+foreverpanda@users.noreply.github.com>
2022-05-14 23:16:30 +0530

    Create code18.cpp

---
+++ Day1/pattern/code18.cpp
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,temp;
+    
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int i=1;
+    
+    while(i<=n)
+    {
+        int j =1;
+        
+        while(j<=i)
+        {
+            char ch = 65 + n-i+j-1;
+            
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+    /*
+Enter number of rows : 4
+D
+CD
+BCD
+ABCD
+
+
+
+
+*/
+
+}
