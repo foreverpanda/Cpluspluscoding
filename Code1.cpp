c7edd13afd4633bcde82e5871ee0ef89f3d22e59
foreverpanda <85871209+foreverpanda@users.noreply.github.com>
2022-05-14 13:50:55 +0530

    Create Code1.cpp
    to check whether entered character is number or alphabet

---
+++ Code1.cpp
@@ -0,0 +1,29 @@
+//to check whether entered character is number or alphabet
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout<<"enter character : ";
+    cin>>ch;
+    
+    
+    if (isalpha(ch))
+    {
+        if(isupper(ch))
+        cout<<"Entered characted is Alphabet and is Uppercase";
+        else
+        cout<<"Entered characted is Alphabet and is Lowercase";
+    }
+    else if (isdigit(ch))
+    {
+        cout<<"Entered characted is a Number";
+    }
+    
+    else
+    cout<<"it is a invalid or a special character";
+    return 0;
+    
+}
